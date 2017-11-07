5cc099645d109446aae83d29157e57e51f649352
ghostbooger <wheiwong@hotmail.com>
2017-11-07 17:44:57 +0000

    Add user microposts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'https://rails-tutorial-rev1-ghostbooger.c9users.io'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
